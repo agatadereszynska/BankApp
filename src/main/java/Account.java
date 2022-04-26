843ccd988a7c9c9416b111380aa03676de5d723f
youagata <youagata@mailinator.com>
2022-04-26 18:30:29 +0200

    add getters and setters for surname and accNumber param

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -5,11 +5,27 @@ public class Account {
     private String accNumber;
     private long balance;
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
     }
 }
