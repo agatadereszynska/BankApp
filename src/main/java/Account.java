a1f9bd5333f5a1002ddadc842ecb1ef0ca55cd9f
youagata <youagata@mailinator.com>
2022-04-26 19:48:43 +0200

    Adding a new method: show account and deposit money. Switch with choice created.

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -51,4 +51,17 @@ public class Account {
         balance = scanner.nextLong();
     }
 
+    public void showAccount() {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Account number: " + accNumber);
+        System.out.println("Balance: " + balance);
+    }
+
+    public void depositMoney() {
+        System.out.println("How much You want to deposit?: ");
+        long amount = scanner.nextLong();
+        balance = balance + amount;
+        System.out.println("Your current balance is: " + balance);
+    }
 }
